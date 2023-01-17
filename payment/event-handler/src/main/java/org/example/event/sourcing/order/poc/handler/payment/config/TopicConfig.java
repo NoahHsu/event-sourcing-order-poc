6b18a9ac260021d7a4c71e308c262459cc5b7aac
NoahHsu <knight6623@gmail.com>
2023-01-17 20:54:47 +0800

    set init topic for shipment and payment

---
+++ payment/event-handler/src/main/java/org/example/event/sourcing/order/poc/handler/payment/config/TopicConfig.java
@@ -0,0 +1,21 @@
+package org.example.event.sourcing.order.poc.handler.payment.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+import static org.example.event.sourcing.order.poc.common.model.event.PaymentEvent.PAYMENT_TOPIC;
+import static org.example.event.sourcing.order.poc.common.model.event.PaymentEvent.PAYMENT_TOPIC_PARTITION;
+
+@Configuration
+public class TopicConfig {
+
+    @Bean
+    public NewTopic paymentEvent() {
+        return TopicBuilder.name(PAYMENT_TOPIC)
+                .partitions(PAYMENT_TOPIC_PARTITION)
+                .build();
+    }
+
+}
