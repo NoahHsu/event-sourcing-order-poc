e063fab9203aff951f913c25a9fd48ebd5400dc6
NoahHsu <knight6623@gmail.com>
2023-02-26 01:41:20 +0800

    fix ObservationRegistry bean not found in client test

--- client/src/test/java/org/example/event/sourcing/order/poc/client/order/OrderCommandMockServerConfig.java
+++ client/src/test/java/org/example/event/sourcing/order/poc/client/order/OrderCommandMockServerConfig.java
@@ -1,6 +1,9 @@
 package org.example.event.sourcing.order.poc.client.order;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.context.annotation.Bean;
 
 public class OrderCommandMockServerConfig {
@@ -9,4 +12,15 @@ public class OrderCommandMockServerConfig {
     public WireMockServer mockOrderCommandServer() {
         return new WireMockServer(8081);
     }
+
+    @Bean
+    ObservationRegistry observationRegistry() {
+        return ObservationRegistry.create();
+    }
+
+    @Bean
+    MeterRegistry meterRegistry() {
+        return new SimpleMeterRegistry();
+    }
+
 }
