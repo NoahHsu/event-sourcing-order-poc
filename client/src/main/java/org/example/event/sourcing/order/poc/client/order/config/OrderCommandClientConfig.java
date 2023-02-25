0e324d52b28588ac21a170017b7e466c33869993
NoahHsu <knight6623@gmail.com>
2023-02-25 22:41:23 +0800

    make api client bring traceId in request

--- client/src/main/java/org/example/event/sourcing/order/poc/client/order/config/OrderCommandClientConfig.java
+++ client/src/main/java/org/example/event/sourcing/order/poc/client/order/config/OrderCommandClientConfig.java
@@ -4,7 +4,12 @@ import feign.Feign;
 import feign.Logger;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import feign.micrometer.MicrometerCapability;
+import feign.micrometer.MicrometerObservationCapability;
 import feign.slf4j.Slf4jLogger;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.annotation.Observed;
 import org.example.event.sourcing.order.poc.client.order.OrderCommandClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +18,16 @@ import org.springframework.context.annotation.Configuration;
 public class OrderCommandClientConfig {
 
     @Bean
-    public OrderCommandClient orderCommandClient() {
+    @Observed
+    public OrderCommandClient orderCommandClient(ObservationRegistry observationRegistry, MeterRegistry meterRegistry) {
         return Feign.builder()
                 .logLevel(Logger.Level.FULL)
                 .logger(new Slf4jLogger())
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
+                .addCapability(new MicrometerObservationCapability(observationRegistry))
+                .addCapability(new MicrometerCapability(meterRegistry))
                 .target(OrderCommandClient.class, "http://localhost:8081");
     }
+
 }
