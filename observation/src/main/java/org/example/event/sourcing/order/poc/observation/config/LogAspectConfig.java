fa4c91c939b6e8d54cb84bb6bb5bf875cc3f5224
NoahHsu <knight6623@gmail.com>
2023-03-25 00:15:23 +0800

    proxyBeanMethods = false

--- observation/src/main/java/org/example/event/sourcing/order/poc/observation/config/LogAspectConfig.java
+++ observation/src/main/java/org/example/event/sourcing/order/poc/observation/config/LogAspectConfig.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 public class LogAspectConfig {
 
     @Bean
