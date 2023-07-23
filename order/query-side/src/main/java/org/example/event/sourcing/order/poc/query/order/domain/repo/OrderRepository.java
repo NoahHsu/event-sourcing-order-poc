12964198a88b0ef1197d5ad9c4aea08e3516503b
NoahHsu <knight6623@gmail.com>
2023-07-23 17:16:37 +0800

    fix backstage embedded swagger-ui try API CORS issue

--- order/query-side/src/main/java/org/example/event/sourcing/order/poc/query/order/domain/repo/OrderRepository.java
+++ order/query-side/src/main/java/org/example/event/sourcing/order/poc/query/order/domain/repo/OrderRepository.java
@@ -4,8 +4,10 @@ import org.example.event.sourcing.order.poc.query.order.domain.entity.OrderRecor
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(path = "v1-orders", collectionResourceRel = "v1-orders", itemResourceRel = "v1-orders")
+@CrossOrigin(origins = "http://localhost:3000") // to allow backstage embeded swaggerUI to try API
 public interface OrderRepository extends JpaRepository<OrderRecord, String> {
 
     @Override
